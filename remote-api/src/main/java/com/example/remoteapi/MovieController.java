3bbddca32e65819da2af42d7af89b0578371d9e8
Sonu Kumar <sonukrsingh998@gmail.com>
2023-09-07 11:05:53 +0530

    calling local api

--- remote-api/src/main/java/com/example/remoteapi/MovieController.java
+++ remote-api/src/main/java/com/example/remoteapi/MovieController.java
@@ -3,6 +3,7 @@ package com.example.remoteapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,6 +25,13 @@ public class MovieController {
         String url="you will get url from remote api along with unique key of api";
         String url1="https://api.themoviedb.org/3/movie/"+movieId+"?api_key=091bc2e9f9b015059c33a5d752f01b90";
         MovieResponse response=restTemplate.getForObject(url1,MovieResponse.class);
+        return response;
+    }
+    //calling local api
+    @GetMapping("/get-student")
+    public Object getStudent(@RequestParam("id"),int regNo){
+        String url="http//localhost/999/student/get/q="+regNo;
+        Object response=restTemplate.getForObject(url,Object.class);
         return response;
     }
 }
